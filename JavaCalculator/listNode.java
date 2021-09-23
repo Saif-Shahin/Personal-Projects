67abc06fa1abf5c1994bb1f16d8f71f9a6e082fd
Saif-Shahin <90293080+Saif-Shahin@users.noreply.github.com>
2021-09-23 15:01:19 -0400

    Create listNode.java

---
+++ JavaCalculator/listNode.java
@@ -0,0 +1,19 @@
+/* @Author: Saif Shahin
+*  This class impliments the linked list data structure.
+*  This code was inspired by ECSE 202 lecture notes (McGill University)
+*/
+
+public class listNode {
+
+//Instance variables of the listNode class
+	String data;
+	listNode next;
+	listNode last;
+
+//Constructor class that will allow for assigning the input Strings to the listNode 'data' instance variable. 
+//I chose to put it here instead of in the Stack and Queue classes as this would void the necessity to repeat the same code 
+//over again. 
+	public listNode(String operator) {
+		data = operator;
+	}
+}
